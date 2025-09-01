a0041b46d649b4787411e5643c9a01564dee8436
Juliano Rodrigues <99824999+jurodri@users.noreply.github.com>
2025-09-01 19:42:43 -0300

    app.py

--- exercicio/app.py
+++ exercicio/app.py
@@ -9,5 +9,5 @@ routes.init_app(app)
 # Se for executado diretamente pelo interpretador
 if __name__ == "__main__":
     # Iniciando o servidor
-    app.run(host="localhost", port=5000, debug=True) 
+    app.run(host="0,0,0,0", port=5000, debug=True) 
     
